76f8563802476e360650290dfce605c3f0adf7a3
Mohammed Fananah <Hp@HP>
2023-02-20 11:18:54 +0200

    Add project files.

---
+++ InventoryManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using InventoryManager.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManager.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Unit> Units { get; set; }
+    }
+}
